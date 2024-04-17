e510fa590dd7de15147a0f5437ad815165a88100
maxsavitsky <maxsavitsky2015@gmail.com>
2024-04-17 10:31:26 +0300

    modify bench distance cpp

--- bench.cpp
+++ bench.cpp
@@ -12,14 +12,25 @@ TEST_CASE("Bench") {
         std::mt19937 rnd(gen());
         std::uniform_int_distribution dist{-100, 100};
 
-        double point_x = dist(rnd);
-        double point_y = dist(rnd);
-        std::array<double, 8> boxes = {};
-        for (int i = 0; i < 8; i++) {
-            boxes[i] = static_cast<double>(dist(rnd));
+        struct TestCase {
+            double point_x;
+            double point_y;
+            std::array<double, 8> boxes;
+        };
+
+        std::vector<TestCase> cases(meter.runs());
+
+        for (int i = 0; i < meter.runs(); i++) {
+            cases[i].point_x = dist(rnd);
+            cases[i].point_y = dist(rnd);
+            for (int j = 0; j < 8; j++) {
+                cases[i].boxes[j] = static_cast<double>(dist(rnd));
+            }
         }
-        meter.measure([&](){
-            return DistancePtoPackedHNB_cpp(point_x, point_y, boxes);
+
+        meter.measure([&](int i){
+            TestCase& test_case = cases[i];
+            return DistancePtoPackedHNB_cpp(test_case.point_x, test_case.point_y, test_case.boxes);
         });
     };
 }
