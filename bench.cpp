2c60d7395b4ce831b024841b9f4ffd0e3fc54057
maxsavitsky <maxsavitsky2015@gmail.com>
2024-04-17 10:35:38 +0300

    add bench distance avx

--- bench.cpp
+++ bench.cpp
@@ -5,32 +5,45 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <random>
 
+struct TestCase {
+    double point_x;
+    double point_y;
+    std::array<double, 8> boxes;
+};
+
+std::vector<TestCase> GenerateTestCases(int n) {
+    std::random_device gen;
+    std::mt19937 rnd(gen());
+    std::uniform_int_distribution dist{-100, 100};
+
+    std::vector<TestCase> cases(n);
+
+    for (int i = 0; i < n; i++) {
+        cases[i].point_x = dist(rnd);
+        cases[i].point_y = dist(rnd);
+        for (int j = 0; j < 8; j++) {
+            cases[i].boxes[j] = static_cast<double>(dist(rnd));
+        }
+    }
+}
+
 
 TEST_CASE("Bench") {
     BENCHMARK_ADVANCED("Bench Distance, CPP")(Catch::Benchmark::Chronometer meter) {
-        std::random_device gen;
-        std::mt19937 rnd(gen());
-        std::uniform_int_distribution dist{-100, 100};
-
-        struct TestCase {
-            double point_x;
-            double point_y;
-            std::array<double, 8> boxes;
-        };
-
-        std::vector<TestCase> cases(meter.runs());
-
-        for (int i = 0; i < meter.runs(); i++) {
-            cases[i].point_x = dist(rnd);
-            cases[i].point_y = dist(rnd);
-            for (int j = 0; j < 8; j++) {
-                cases[i].boxes[j] = static_cast<double>(dist(rnd));
-            }
-        }
+        auto cases = GenerateTestCases(meter.runs());
 
         meter.measure([&](int i){
             TestCase& test_case = cases[i];
             return DistancePtoPackedHNB_cpp(test_case.point_x, test_case.point_y, test_case.boxes);
+        });
+    };
+
+    BENCHMARK_ADVANCED("Bench Distance, AVX")(Catch::Benchmark::Chronometer meter) {
+        auto cases = GenerateTestCases(meter.runs());
+
+        meter.measure([&](int i){
+            TestCase& test_case = cases[i];
+            return DistancePtoPackedHNB_avx(test_case.point_x, test_case.point_y, test_case.boxes);
         });
     };
 }
